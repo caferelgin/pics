75f5fd1d330c3ae589642cc04f1827ce2beca13c
Cafer Elgin <cafer.elgin@gmail.com>
2021-10-11 19:55:15 +0300

    initial commit

---
+++ lib/src/models/image_model.dart
@@ -0,0 +1,13 @@
+class ImageModel {
+  late int id;
+  late String title;
+  late String url;
+
+  ImageModel(this.id, this.title, this.url);
+
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    title = parsedJson['title'];
+    url = parsedJson['url'];
+  }
+}
